d28a9c6223e1b7073bcf189eff165f7666be7a2c
vtu27375-hub <vtu27375@veltech.edu.in>
2025-10-24 19:04:05 +0530

    Task 1.1.py

---
+++ Task 1.1.py
@@ -0,0 +1,36 @@
+graph = {
+'5' : ['3','7'],
+'3' : ['2', '4'],
+'7' : ['8'],
+'2' : [],
+'4' : ['8'],
+'8' : []
+}
+
+visited = [] # List for visited nodes.
+queue = []     #Initialize a queue
+
+defbfs(visited, graph, node): #function for BFS
+visited.append(node)
+queue.append(node)
+
+  while queue:         
+    m = queue.pop(0) 
+graph = {
+'5' : ['3','7'],
+'3' : ['2', '4'],
+'7' : ['8'],
+'2' : [],
+'4' : ['8'],
+'8' : []
+}
+
+visited = [] # List for visited nodes.
+queue = []     #Initialize a queue
+
+defbfs(visited, graph, node): 
+visited.append(node)
+queue.append(node)
+
+  while queue:         
+    m = queue.pop(0) 
